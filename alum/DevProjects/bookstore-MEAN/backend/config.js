f2ee711f33865af0580bb5d5e8a46437beeca448
Vicente Hoyos <vhoyospoyato@gmail.com>
2025-10-03 00:09:03 +0200

    First commit

---
+++ alum/DevProjects/bookstore-MEAN/backend/config.js
@@ -0,0 +1,4 @@
+export  const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://root:X71eFD1lMcJgsiYm@bookstore-mern.r3aml.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN'
---
+++ alum/DevProjects/bookstore-MEAN/backend/routes/booksRoute.js
@@ -0,0 +1,106 @@
+import express from 'express';
+import { Book } from '../models/bookModels.js';
+
+const router = express.Router();
+
+//New book
+router.post('/', async (request, response)=>{
+    try{
+        if(
+            !request.body.title||
+            !request.body.author||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+
+    } catch (error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Get all info
+router.get('/', async(request,response)=> {
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data:books
+        });
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Get one point of data
+router.get('/:id', async(request,response)=> {
+    try {
+
+        const {id} = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Update data for specific point
+router.put('/:id', async(request,response)=>{
+    try {
+        if(
+            !request.body.title||
+            !request.body.author||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const {id} = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result){
+            return response.status(404).json({message:'Book not found'});
+        }
+        return response.status(200).send({message:'Book updated succesfully'});
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message}); 
+    }
+});
+
+//Delete info
+router.delete('/:id', async(request,response)=>{
+    try {
+        const {id} = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(404).json({message:'Book not found'});
+        }
+        return response.status(200).send({message: 'Book susccesfully deleted'});
+    } catch (error) {
+        console.log(error);
+        response.status(500).send({message:error.message});
+    }
+});
+
+export default router;
