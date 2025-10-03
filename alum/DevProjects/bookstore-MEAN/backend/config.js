7c993a126ad4fcca42596228a0bcc3ba82a4e069
Vicente Hoyos <vhoyospoyato@gmail.com>
2025-10-03 09:37:19 +0200

    First commit 2.0

--- alum/DevProjects/bookstore-MEAN/backend/config.js
+++
@@ -1,4 +0,0 @@
-export  const PORT = 5555;
-
-export const mongoDBURL =
-    'mongodb+srv://root:X71eFD1lMcJgsiYm@bookstore-mern.r3aml.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN'
